2029d5ad6f6a3617d12174918d9836d4675cab87
David McClure <davidwilliammcclure@gmail.com>
2017-01-24 16:16:54 -0800

    Index tokens.

--- src/main/scala/litcene/Litcene.scala
+++ src/main/scala/litcene/Litcene.scala
@@ -4,17 +4,18 @@ package litcene
 
 import java.io.File
 
-import scala.collection.mutable.HashMap
+import scala.collection.mutable.{HashMap,ArrayBuffer}
 import scala.io.Source
 
 import org.apache.commons.io.FilenameUtils
 
 
-// tokenize text, with ratios
-// index text - id, text
-// basic concordance
-
-case class Token(token: String, start: Int, end: Int, offset: Double)
+case class Token(
+  token: String,
+  start: Int,
+  end: Int,
+  offset: Double
+)
 
 case class Tokenizer(regex: String = "[a-z]+") {
 
@@ -30,14 +31,34 @@ case class Tokenizer(regex: String = "[a-z]+") {
 
 }
 
+case class IndexToken(
+  docId: String,
+  token: String,
+  start: Int,
+  end: Int,
+  offset: Double
+)
+
 class Index {
 
   val tokenizer = new Tokenizer
 
   val documents: HashMap[String, String] = HashMap.empty
 
+  val tokens: ArrayBuffer[IndexToken] = ArrayBuffer.empty
+
   def index(id: String, text: String) {
+
     documents(id) = text
+
+    val docTokens = for (t <- tokenizer(text)) yield {
+      IndexToken(id, t.token, t.start, t.end, t.offset)
+    }
+
+    tokens ++= docTokens
+
+    println(id, docTokens.length)
+
   }
 
   def indexFile(id: String, path: String) {
